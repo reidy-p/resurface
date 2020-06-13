9f8b739fc96804d02001c219fd11bc3ae4af9fac
reidy-p <paul_reidy@outlook.com>
2020-06-13 23:28:48 +0100

    Update email code to work on AWS Lambda

--- weeklypocket/lambda_function.py
+++ weeklypocket/lambda_function.py
@@ -1,35 +1,21 @@
 import pickle
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 from email.mime.text import MIMEText
 import base64
 from urllib.error import HTTPError
+import boto3
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/gmail.send']
 
-def gmail_authenticate():
-    """Shows basic usage of the Gmail API."""
-    creds = None
+def get_gmail_token(bucket, file_name):
+    """Read the saved gmail tokens from S3"""
     # The file gmail_token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('gmail_token.pickle'):
-        with open('gmail_token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'gmail_credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('gmail_token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+
+    creds = pickle.loads(boto3.client('s3').get_object(Bucket=bucket, Key=file_name)['Body'].read())
+
+    # TODO: Check whether creds are valid using creds.valid
 
     return build('gmail', 'v1', credentials=creds)
 
@@ -72,8 +58,15 @@ def send_message(service, user_id, message):
   except HTTPError as error:
     print('An error occurred: %s' % error)
 
-if __name__ == '__main__':
-    service = gmail_authenticate()
-    msg = create_message("me", "paul_reidy@outlook.com", "Weekly Pocket", "Hi Paul,\nThis is your Weekly Pocket e-mail. Click on the link below to view a randomly chosen e-mail from your Pocket favourites.\n\nhttp://127.0.0.1:5000")
+def my_handler(recipient, context):
+    bucket = "weekly-pocket"
+    file_name = "gmail_token.pickle"
+    
+    service = get_gmail_token(bucket, file_name)
+    msg = create_message("me", recipient, "Weekly Pocket", "Hi Paul,\nThis is your Weekly Pocket e-mail. Click on the link below to view a randomly chosen e-mail from your Pocket favourites.\n\nhttp://127.0.0.1:5000")
     send_message(service, "me", msg)
+
+    return { 
+        'message' : 'ok'
+    }  
 
