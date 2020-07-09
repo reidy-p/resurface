f3305dc4a5c9f63f05f05fe9250b23349dc756cc
reidy-p <paul_reidy@outlook.com>
2020-07-09 16:01:16 +0100

    First version of landing page and user registration/login

--- resurface/__init__.py
+++ resurface/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask, session
 from flask_executor import Executor
-import yaml
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
+from config import Config
 
 application = Flask(__name__)
+application.config.from_object(Config)
+db = SQLAlchemy(application)
+login = LoginManager(application)
 executor = Executor(application)
+bootstrap = Bootstrap(application)
 
-with open("config.yml", 'r') as keys_file:
-    config = yaml.safe_load(keys_file)
-
-application.secret_key = config['secret_key']
-
-from resurface import routes
+from resurface import routes, models
+db.create_all()
+db.session.commit()
--- resurface/routes.py
+++ resurface/routes.py
@@ -1,38 +1,75 @@
 from flask import render_template, url_for, redirect, request, send_file, Response
 import requests
-from resurface import application
+from resurface import application, db
 import webbrowser
-import yaml
 import random
-from flask import session
+from flask import session, flash
+from flask_login import current_user, login_user, logout_user
+from resurface.models import User
+from resurface.forms import LoginForm, RegistrationForm
 
-with open("config.yml", 'r') as keys_file:
-    config = yaml.safe_load(keys_file)
+@application.route('/')
+def index():
+    return render_template("index.html")
 
-CONSUMER_KEY = config['consumer_key']
+@application.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
-@application.route("/")
+@application.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('home'))
+    return render_template('login.html', title='Sign In', form=form)
+
+@application.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@application.route("/home")
 def home():
     headers = {'Content-Type': "application/json; charset=UTF-8", "X-Accept": "application/json"}
     redirect_uri = url_for("callback", _external=True)
-    data = {"redirect_uri": redirect_uri, "consumer_key": CONSUMER_KEY}
+    data = {"redirect_uri": redirect_uri, "consumer_key": application.config['CONSUMER_KEY']}
     r = requests.post("https://getpocket.com/v3/oauth/request", headers=headers, json=data)
     authorization_code = r.json()['code']
 
     session['authorization_code'] = authorization_code
     url = f"https://getpocket.com/auth/authorize?request_token={authorization_code}&redirect_uri={redirect_uri}"
-    return render_template('login.html', redirect_url=url)
+    return render_template('home.html', redirect_url=url)
 
 @application.route("/callback")
 def callback():
     headers = {'Content-Type': "application/json; charset=UTF-8", "X-Accept": "application/json"}
-    data = {"code": session['authorization_code'], "consumer_key": CONSUMER_KEY}
+    data = {"code": session['authorization_code'], "consumer_key": application.config['CONSUMER_KEY']}
     r = requests.post("https://getpocket.com/v3/oauth/authorize", headers=headers, json=data)
 
     access_token = r.json()['access_token']
-    data = {"access_token": access_token, "consumer_key": CONSUMER_KEY, "favorite": 1}
+    data = {"access_token": access_token, "consumer_key": application.config['CONSUMER_KEY'], "favorite": 1}
     r = requests.get("https://getpocket.com/v3/get/", json=data)
     favourites = r.json()['list']
     choice = random.choice(list(favourites.keys()))
     return redirect(favourites[choice]["resolved_url"])
 
+@application.route("/carousel")
+def carousel():
+    return render_template('carousel.html')
