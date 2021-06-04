d1c6e4858ef09ee90d156522f34e1bfbe739c847
Paul Reidy <paul-reidy@outlook.com>
2021-06-04 21:31:47 +0100

    Only create one db file

--- resurface/__init__.py
+++ resurface/__init__.py
@@ -4,9 +4,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from config import Config
+import os
 
 application = Flask(__name__)
 application.config.from_object(Config)
+
+# get the path for the route folder of the app
+base_path = os.path.dirname(os.path.dirname(__file__))
+application.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{base_path}/resurface.db'
+
 db = SQLAlchemy(application)
 login = LoginManager(application)
 executor = Executor(application)
